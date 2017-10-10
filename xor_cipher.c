744f86d1afac7cba24e2d05f4f4186bae9848935
Juan Carlos Lanuza Lazo <jclanuza@icloud.com>
2017-10-09 22:44:02 -0600

    Update
    segmentation of funtion

--- xor_cipher.c
+++ xor_cipher.c
@@ -1,41 +1,39 @@
 /* A very simple and little example of a XOR Cipher 
-
 	Juan Carlos Lanuza L. - MGA - 2016
 */
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main(void)
+unsigned xorcipher(char *xor_string,unsigned key);
+
+int main(int argc, char** argv)
 {
-	int i,key;
-	char c_string[64],xor_string[64];
+	unsigned key;
+	char c_string[64];
+	
 	printf("String:");
-		gets(c_string);
-	while(strlen(c_string) > 64 )
-	{
-		puts("Sorry, the max value is 64 characteres, try again with a more short string");
-		printf("String: ");
-			scanf("%s",c_string);
-	}
-	printf("XOR Key (0 - 255):");
-		scanf("%d",&key);
-	while(key < 0 || key > 255)
-	{
-		puts("The Max values for the key in this case of implementation is (0 - 255)\n");
-		printf("XOR Key: ");	
-		scanf("%d",key);	
-	}			
-	for(i = 0; i <= strlen(c_string); i++)
+	gets(c_string);
+	fprintf(stdout,"XOR Key (0 - 255):");
+	scanf("%d",&key);		
+	xorcipher(c_string,key);
+	return 0;
+}
+
+unsigned xorcipher(char *c_string,unsigned key)
+{
+	char xor_string[64];
+	for(int i = 0;i <= strlen(c_string);i++)
 	{
 		xor_string[i] = (char)((int)c_string[i] ^ key);	
 	}	
-	puts("");
-	puts("");
-	puts("------------------------------------");
-	printf("Current String: %s\n\n",c_string);
-	printf("Ciphered String: %s\n\n",xor_string);
-	printf("Ciphered String HEX: %X\n\n",xor_string);
-	puts("------------------------------------");
-exit(EXIT_SUCCESS); /* only for OS, no valid for Microcontrolers or Microprocessors*/
+	fprintf(stdout,
+			"\n------------------------------------\n"
+			"Current String: %s\n"
+			"XOR Key: %d\n"
+			"Ciphered String: %s\n"
+			"Ciphered String HEX: %X\n"
+			"------------------------------------\n"
+			,c_string,key,xor_string,xor_string
+			);
+	return 0;	
 }
